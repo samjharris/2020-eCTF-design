f9478051a272e2dd299f5dba21ca3ef004478875
rlee287 <rlee287@yahoo.com>
2020-04-19 22:18:41 -0700

    Remove system.bd when creating new project before running Vivado batch script
    (cherry picked from commit 5d0e78fab74cd966116677a78857d33d7a04f12f)

--- tools/buildBitstream.py
+++ tools/buildBitstream.py
@@ -27,6 +27,9 @@ def create_project(vivado_batch, path_to_proj_tcl, proj_name):
     """ Invoke Vivado batch mode to run create_project.tcl. Pre-cond: pl-repo root"""
 
     # vivado batch mode
+    proj_bd = dev_path_pl + "/src/bd/system/system.bd"
+    if os.path.isfile(proj_bd):
+        os.unlink(proj_bd)
     src_create = vivado_batch + path_to_proj_tcl + " -tclargs --project_name " + proj_name
     print("Running: " + src_create + "\n")
     os.system(src_create)
